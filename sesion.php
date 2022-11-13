7068de50e611474db3940ee7ad77558ebe717b4d
LAJEN16 <118131391+LAJEN16@users.noreply.github.com>
2022-11-13 14:24:29 -0500

    Add files via upload

---
+++ sesion.php
@@ -0,0 +1,48 @@
+<?PHP
+$hostname="192.168.1.56";
+$database="usuarios";
+$username="admin";
+$password="123456";
+
+$json=array();
+	if(isset($_GET["user"]) && isset($_GET["pwd"])){
+		$user=$_GET['user'];
+		$pwd=$_GET['pwd'];
+		
+		
+		
+		$conexion=mysqli_connect($hostname,$username,$password,$database);
+		
+		
+		
+		$consulta="SELECT user, pwd, names FROM users WHERE user= '{$user}' AND pwd = '{$pwd}'";
+		$resultado=mysqli_query($conexion,$consulta);
+
+		if($consulta){
+		
+			if($reg=mysqli_fetch_array($resultado)){
+				$json['datos'][]=$reg;
+			}
+			mysqli_close($conexion);
+			echo json_encode($json);
+		}
+
+
+
+		else{
+			$results["user"]='';
+			$results["pwd"]='';
+			$results["names"]='';
+			$json['datos'][]=$results;
+			echo json_encode($json);
+		}
+		
+	}
+	else{
+		   	$results["user"]='';
+			$results["pwd"]='';
+			$results["names"]='';
+			$json['datos'][]=$results;
+			echo json_encode($json);
+		}
+?>
